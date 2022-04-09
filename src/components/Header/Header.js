89b542df8be86d518523ec3418a68253bd2abaf2
leyksnal <leyksnal@gmail.com>
2022-04-10 00:43:45 +0100

    Home component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,76 @@
 import React from 'react'
 import styled from 'styled-components'
+import {HiDotsVertical} from 'react-icons/hi'
 
 export default function Header() {
   return (
-    <Container>
-      <Wrapper>
-        <Logo>
-          <img src="/img/logo.svg" alt="" />
-        </Logo>
-        <Nav>
-          <span>Home</span>
-          <span>Explore</span>
-          <span>Engagement</span>
-          <span>About us</span>
-          <button>Try free</button>
-        </Nav>
-        <Bugger></Bugger>
-      </Wrapper>
-    </Container>
+    <>
+      <Container>
+        <Wrapper>
+          <Logo>
+            <img src="/img/logo.svg" alt="" />
+          </Logo>
+          <Nav>
+            <span>Home</span>
+            <span>Explore</span>
+            <span>Engagement</span>
+            <span>About us</span>
+            <Button>Try free</Button>
+          </Nav>
+        </Wrapper>
+        <Bugger>
+          <HiDotsVertical/>
+        </Bugger>
+      </Container>
+
+    </>
+
   )
 }
 
 const Container = styled.div`
-  background-color: brown;
+  /* background-color: brown; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90px;
 `;
 const Wrapper = styled.div`
-
+  width: 90%;
+  /* background-color: yellowgreen; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 const Logo = styled.div`
-
+  /* background-color: black; */
+  img{
+    object-fit: contain;
+    width: 200px;
+    height: 40px;
+  }
 `;
 const Bugger = styled.div`
 
 `;
+const Button = styled.button`
+    width: 150px;
+    height: 40px;
+    outline: none;
+    border: none;
+    background-color: red;
+    border-radius: 30px;
+    color: gray;
+`;
 const Nav = styled.div`
-
+  /* background-color: #fff; */
+  width: 700px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+  span{
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
 `;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,9 +6,54 @@ export default function Home() {
   return (
     <Container>
         <Header/>
+        <Wrapper>
+          <Left>
+            <h2>Build The Community Your Fans Will Love</h2>
+            <span>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. Get Started For Free Grow Together Generate meaningful discussions</span>
+            <Button>Try free</Button>
+          </Left>
+          <Right>
+            <img src="/img/mockups.svg" alt="" />
+          </Right>
+        </Wrapper>
     </Container>
   )
 }
 
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-image: url("/img/bg-hero.svg");
+  width: 100%;
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const Wrapper = styled.div`
+  background-color: blue;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+
+const Left = styled.div`
+
+`;
+
+
+const Right = styled.div`
+
+`;
+
+const Button = styled.button`
+    width: 150px;
+    height: 40px;
+    outline: none;
+    border: none;
+    background-color: red;
+    border-radius: 30px;
+    color: gray;
+`;
